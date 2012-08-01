839e9e6bfccf9cd53404e1b7a817356db9ae140c
unknown <jffu@jffu-PC.(none)>
2012-08-01 17:36:58 +0800

    centimeter plus meter

---
+++ OOWorkshop/Centimeter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOWorkshop
+{
+    class Centimeter
+    {
+        public Centimeter(double number)
+        {
+        }
+
+        protected double Number { get; private set; }
+
+        public static Centimeter operator+(Centimeter cm1, Centimeter cm2)
+        {
+            return new Centimeter(cm1.Number + cm2.Number);
+        }
+
+        public static Centimeter operator+(Centimeter cm, Decimeter dm)
+        {
+            return new Centimeter(cm.Number + 10 * dm.Number);
+        }
+
+        public static Centimeter operator+(Centimeter cm, Meter m)
+        {
+            return new Centimeter(cm.Number + m.Number * 100);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var centimeter = obj as Centimeter;
+            return centimeter.Number.Equals(Number);
+        }
+
+        public override int GetHashCode()
+        {
+            return Number.GetHashCode();
+        }
+    }
+}
---
+++ OOWorkshopTest/CentimeterTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using OOWorkshop;
+
+namespace OOWorkshopTest
+{
+    [TestFixture]
+    class CentimeterTest
+    {
+        private Centimeter oneCM;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            oneCM = new Centimeter(1);
+        }
+
+        [Test]
+        public void should_equal_2cm_given_1cm_plus_1cm()
+        {
+            Assert.AreEqual(new Centimeter(2), oneCM + oneCM);
+        }
+
+        [Test]
+        public void should_equal_11_centimeters_given_1_centimeter_and_1_decimeter()
+        {
+            var oneDM = new Decimeter(1);
+            Assert.AreEqual(new Centimeter(11), oneCM + oneDM);
+        }
+
+        [Test]
+        public void should_equal_101_centimeters_given_1_centimeter_and_1_meter()
+        {
+            var oneM = new Meter(1);
+            Assert.AreEqual(new Centimeter(101), oneCM + oneM);
+        }
+    }
+}

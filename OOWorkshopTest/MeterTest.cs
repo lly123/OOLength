1189c793491a4ab4861fde4c10975e99c73b879b
unknown <jffu@jffu-PC.(none)>
2012-08-01 15:12:12 +0800

    refactor test - added setup

--- OOWorkshopTest/MeterTest.cs
+++ OOWorkshopTest/MeterTest.cs
@@ -10,36 +10,40 @@ namespace OOWorkshopTest
     [TestFixture]
     public class MeterTest
     {
+        private Meter oneMeter;
+        private Meter twoMeters;
+        private Meter threeMeters;
+
+        [TestFixtureSetUp]
+        public void setUp()
+        {
+            oneMeter = new Meter(1);
+            twoMeters = new Meter(2);
+            threeMeters = new Meter(3);
+        }
+
         [Test]
         public void should_equal_2m_given_1m_plus_1m()
         {
-            var oneMeter = new Meter(1);
-            var twoMeters = new Meter(2);
             Assert.AreEqual(twoMeters, oneMeter + oneMeter);
         }
 
         [Test]
         public void should_not_equal_2m_given_3m_plus_1m()
         {
-            var threeMeters = new Meter(3);
-            var oneMeter = new Meter(1);
-            Assert.AreNotEqual(new Meter(2), threeMeters + oneMeter);
+            Assert.AreNotEqual(twoMeters, threeMeters + oneMeter);
         }
 
         [Test]
         public void should_equal_1m_given_2m_minus_1m()
         {
-            var twoMeters = new Meter(2);
-            var oneMeter = new Meter(1);
-            Assert.AreEqual(new Meter(1), twoMeters - oneMeter);
+            Assert.AreEqual(oneMeter, twoMeters - oneMeter);
         }
 
         [Test]
         public void should_not_equal_1m_given_3m_minus_1m()
         {
-            var threeMeters = new Meter(3);
-            var oneMeter = new Meter(1);
-            Assert.AreNotEqual(new Meter(1), threeMeters - oneMeter);
+            Assert.AreNotEqual(oneMeter, threeMeters - oneMeter);
         }
     }
 }

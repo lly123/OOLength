a2a11981fed6ee82251579bc3ad40e5caa02c406
unknown <jffu@jffu-PC.(none)>
2012-08-01 14:44:57 +0800

    added 1m plus 1m equal 2m & 1m plus 2m not equal 2m

---
+++ OOWorkshop/Meter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOWorkshop
+{
+    public class Meter
+    {
+        private int number;
+
+        public Meter(int number)
+        {
+            this.number = number;
+        }
+
+        public Meter Plus(Meter oneMeter)
+        {
+            return new Meter(1);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var meter = obj as Meter;
+            return meter.number == number;
+        }
+
+
+    }
+}
---
+++ OOWorkshopTest/MeterTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using OOWorkshop;
+
+namespace OOWorkshopTest
+{
+    [TestFixture]
+    public class MeterTest
+    {
+        [Test]
+        public void should_equal_2m_given_1m_plus_1m()
+        {
+            var oneMeter = new Meter(1);
+            var twoMeter = new Meter(2);
+            var result = oneMeter.Plus(oneMeter);
+            Assert.AreEqual(twoMeter, result);
+        }
+
+        [Test]
+        public void should_not_equal_2m_given_3m_plus_1m()
+        {
+            var threeMeter = new Meter(3);
+            var oneMeter = new Meter(1);
+            Assert.AreNotEqual(new Meter(2), threeMeter.Plus(oneMeter));
+        }
+    }
+}
